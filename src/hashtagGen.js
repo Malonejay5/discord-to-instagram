dbd3333ee4bfd60ecaa7fa00206845ff9b380fa6
Malonejay5 <103909037+Malonejay5@users.noreply.github.com>
2023-11-14 00:12:42 -0800

    Update hashtagGen.js

--- src/hashtagGen.js
+++ src/hashtagGen.js
@@ -1,58 +1,38 @@
-const axios = require('axios');
-require('dotenv').config()
+let getHash1 = async (keyWord) => {
+  try {
+      let link = 'https://hashtagy-generate-hashtags.p.rapidapi.com/v1/insta/tags';
+      let req = await axios.get(link, {
+          params: {
+              keyword: keyWord,
+              include_tags_info: 'true'
+          },
+          headers: {
+              'X-RapidAPI-Key': apiKey,
+              'X-RapidAPI-Host': 'hashtagy-generate-hashtags.p.rapidapi.com'
+          }
+      });
 
-let apiKey = process.env.API_KEY
-
-let getHash = async (keyWord) => {
-    const options = {
-        method: 'GET',
-        url: 'https://hashtagy-generate-hashtags.p.rapidapi.com/v1/insta/tags',
-        params: {
-          keyword: keyWord,
-          include_tags_info: 'true'
-        },
-        headers: {
-          'X-RapidAPI-Key': apiKey,
-          'X-RapidAPI-Host': 'hashtagy-generate-hashtags.p.rapidapi.com'
-        }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          let jsonData = JSON.stringify(response.data)
-          let jsonParse = JSON.parse(jsonData).data
-          let hashTags = jsonParse['hashtags']['hashtag']
-          //let jsonData = JSON.parse(response.data)
-          console.log(hashTags);
-      } catch (error) {
-          console.error(error);
+      let hashTagArray = [];
+      let jsonData = JSON.stringify(req.data);
+      let jsonParse = JSON.parse(jsonData).data;
+      let hashTags = jsonParse['hashtags'];
+      for (let i = 0; i < hashTags.length; i++) {
+          if(i < 15){
+            hashTagArray.push(`#${hashTags[i]['hashtag']}`);
+          }
+         
       }
+
+      return hashTagArray;
+  } catch (err) {
+      console.log(err);
+  }
 }
 
-getHash('travel')
+let testing = async (keyWord) => {
+  let hashtags = await getHash1(keyWord);
+  console.log(hashtags);
+}
 
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// let getHash = async (keyWord) => {
-//     let link = 'https://hashtagy-generate-hashtags.p.rapidapi.com/v1/insta/tags'
-//     let req = await axios.get(link,{
-//         params: {
-//             keyword: 'tattoo',
-//         include_tags_info: 'true'
-//         },
-//         headers: {
-//             'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-//             'X-RapidAPI-Host': 'hashtagy-generate-hashtags.p.rapidapi.com'
-//         }
-//     })
-//     .then((res) => {
-//         console.log(res.data)
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
-// }
-
-
-// getHash('tattoo')
+exports.getHashTags = testing
